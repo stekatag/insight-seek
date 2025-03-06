b0eceb42a13ab90326be91ba9fe8c6663924bb40
stekatag <stefangogov@proton.me>
2025-03-06 23:10:15 +0200

    update sign-in component to redirect to dashboard

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return (
-    <SignIn
-      path={process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL}
-      forceRedirectUrl="/sync-user"
-    />
-  );
+  return <SignIn forceRedirectUrl="/dashboard" />;
 }
