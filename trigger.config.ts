3d4d7c73f1f4ade2a743654b3c64d1cacd9d130e
Stefan Gogov <stefangogov@proton.me>
2026-08-26 22:00:08 +0300

    feat: update runtime in trigger configuration to node-24 for compatibility

--- trigger.config.ts
+++ trigger.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@trigger.dev/sdk";
 
 export default defineConfig({
   project: "proj_klbsmzqtlohurhatqovt",
-  runtime: "node",
+  runtime: "node-24",
   logLevel: "log",
   // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
   // You can override this on an individual task.
